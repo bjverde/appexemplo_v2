c48b4a622fcae60a2d7bb952901c6e08765e77dc
Bjverde <bjverde@yahoo.com.br>
2020-01-26 01:01:25 -0300

    :hammer: inclusão da nova tela

---
+++ app/control/telas/RegiaoForm.php
@@ -0,0 +1,76 @@
+<?php
+
+class RegiaoForm extends TPage
+{
+    protected $form;      // form
+    protected $datagrid;  // datagrid
+    protected $loaded;
+    protected $pageNavigation;  // pagination component
+    
+    // trait with onSave, onEdit, onDelete, onReload, onSearch...
+    use Adianti\Base\AdiantiStandardFormListTrait;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->setDatabase('form_exemplo'); // define the database
+        $this->setActiveRecord('Regiao'); // define the Active Record
+        $this->setDefaultOrder('cod_regiao', 'asc'); // define the default order
+        $this->setLimit(-1); // turn off limit for datagrid
+        
+        // create the form
+        $this->form = new BootstrapFormBuilder('form_regiao');
+        $this->form->setFormTitle(_t('Região'));
+
+        // create the form fields
+        $id     = new TEntry('cod_regiao');
+        $name   = new TEntry('nom_regiao');
+        
+        // add the form fields
+        $this->form->addFields( [new TLabel('ID', 'red')],    [$id] );
+        $this->form->addFields( [new TLabel('Nome', 'red')],  [$name] );
+        
+        $name->addValidation('Name', new TRequiredValidator);
+        
+        // define the form actions
+        $this->form->addAction( 'Save', new TAction([$this, 'onSave']), 'fa:save green');
+        $this->form->addActionLink( 'Clear',new TAction([$this, 'onClear']), 'fa:eraser red');
+
+        // make id not editable
+        $id->setEditable(FALSE);
+        
+        // create the datagrid
+        $this->datagrid = new BootstrapDatagridWrapper(new TDataGrid);
+        $this->datagrid->width = '100%';
+        
+        // add the columns
+        $col_id    = new TDataGridColumn('cod_regiao', 'Id', 'right', '10%');
+        $col_name  = new TDataGridColumn('nom_regiao', 'Nome', 'left', '90%');
+        
+        $this->datagrid->addColumn($col_id);
+        $this->datagrid->addColumn($col_name);
+        
+        $col_id->setAction( new TAction([$this, 'onReload']),   ['order' => 'cod_regiao']);
+        $col_name->setAction( new TAction([$this, 'onReload']), ['order' => 'nom_regiao']);
+        
+        // define row actions
+        $action1 = new TDataGridAction([$this, 'onEdit'],   ['key' => '{cod_regiao}'] );
+        $action2 = new TDataGridAction([$this, 'onDelete'], ['key' => '{cod_regiao}'] );
+        
+        $this->datagrid->addAction($action1, 'Edit',   'far:edit blue');
+        $this->datagrid->addAction($action2, 'Delete', 'far:trash-alt red');
+        
+        // create the datagrid model
+        $this->datagrid->createModel();
+
+        // wrap the page content using vertical box
+        $vbox = new TVBox;
+        $vbox->style = 'width: 100%';
+        $vbox->add(new TXMLBreadCrumb('menu.xml', __CLASS__));
+        $vbox->add($this->form);
+        $vbox->add(TPanelGroup::pack('', $this->datagrid));
+        
+        parent::add($vbox);
+    }
+}
