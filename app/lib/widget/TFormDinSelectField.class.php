3c70f2e5d165b5ede618ba67b8954e25e9e49830
Bjverde <bjverde@yahoo.com.br>
2020-01-27 00:10:23 -0300

    :bug: correção de falhas

--- app/lib/widget/TFormDinSelectField.class.php
+++ app/lib/widget/TFormDinSelectField.class.php
@@ -12,12 +12,14 @@ class TFormDinSelectField
      * @param string $strLabel      - 2: Label do campo
      * @param boolean $boolRequired - 3: Obrigatorio
      * @param array $mixOptions     - 4: array dos valores. no formato "key=>value"
+     * @return TCombo
      */
     public function __construct(string $id,string $strLabel,$boolRequired = false, array $mixOptions)
     {
         $this->adiantiObj = new TCombo($id);
         $this->adiantiObj->addItems($mixOptions);
         if($boolRequired){
+            $strLabel = empty($strLabel)?$id:$strLabel;
             $this->adiantiObj->addValidation($strLabel, new TRequiredValidator);
         }
         return $this->getAdiantiObj();
