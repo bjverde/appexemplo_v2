3fde218f1d2dc80571d65347da56cd5fca3d46e6
Bjverde <bjverde@yahoo.com.br>
2020-02-03 21:38:21 -0300

    :hammer: FormDin

--- app/lib/widget/TFormDin.class.php
+++ app/lib/widget/TFormDin.class.php
@@ -26,4 +26,38 @@ class TFormDin
     public function getAdiantiObj(){
         return $this->adiantiObj;
     }
+
+    /**
+     * Inclusão 
+     * @param array $label - label que será incluido com o campo
+     * @param array $campo - campo que será incluido
+     */
+    public function addFields(array $label, array $campo){
+        $this->adiantiObj->addFields($label, $campo);
+    }
+
+    /**
+     * Campo de entrada de dados texto livre
+     * Reconstruido FormDin 4 Sobre o Adianti 7
+     *
+     * @param string $id            - 1: ID do campo
+     * @param string $strLabel      - 2: Label do campo, usado para validações
+     * @param integer $intMaxLength - 3: Tamanho máximo de caracteres
+     * @param boolean $boolRequired - 4: Obrigatorio. DEFAULT = False.
+     * @param string $strValue      - 5: Texto preenchido ou valor default
+     * @param string $strExampleText- 6: Texto de exemplo ou placeholder 
+     * @return TEntry
+     */
+    public function addTextField(string $id
+                                ,string $strLabel
+                                ,int $intMaxLength = null
+                                ,$boolRequired = false
+                                ,string $strValue=null
+                                ,string $strExampleText =null){
+        $formDinTextField = new TFormDinTextField($id,$strLabel,$intMaxLength,$boolRequired,$strValue,$strExampleText);
+        $objField = $formDinTextField->getAdiantiObj();
+        $formDinLabelField = new TFormDinLabelField($strLabel,$boolRequired);
+        $label = $formDinLabelField->getAdiantiObj();
+        $this->addFields([$label], [$objField]);
+    }
 }
