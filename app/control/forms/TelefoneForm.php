bb4e8192c0ad33375b1d85d663d41987b6dc66f4
Bjverde <bjverde@yahoo.com.br>
2020-02-08 19:42:58 -0300

    :bug: tentando fazer funcionar

--- app/control/forms/TelefoneForm.php
+++ app/control/forms/TelefoneForm.php
@@ -12,13 +12,16 @@ class TelefoneForm extends TPage
         $this->form = new BootstrapFormBuilder(__CLASS__);
         $this->form->setFormTitle('Testa telefone');
         
+        $codLabel = 'Codigo Area';
+        $formDinCod = new TFormDinNumericField('code',$codLabel,'2',true,2,null,null,00,99);
+        $cod = $formDinCod->getAdiantiObj();
+
         $telefoneLabel = 'Telefone';
         $formDinTextField = new TFormDinFoneField('tel',$telefoneLabel,true);
-        $telefone = $formDinTextField->getAdiantiObj();    
+        $telefone = $formDinTextField->getAdiantiObj();
         
         // add the form fields
-        //$this->form->addFields( [new TLabel('Cod', 'red')],    [$id] );
-        $this->form->addFields( [new TLabel($telefoneLabel, 'red')],  [$telefone] );
+        $this->form->addFields( [new TLabel($codLabel, 'red')],    [$cod], [new TLabel($telefoneLabel, 'red')],  [$telefone] );
         
         // define the form actions
         $this->form->addAction( 'Buscar', new TAction([$this, 'onTelefone']), 'fa:save green');
@@ -31,19 +34,19 @@ class TelefoneForm extends TPage
         parent::add($vbox);
     }
 
-    public static function onTelefone($param)    
-    {   
-        var_dump($param);
+    public function onTelefone($param)    
+    {           
         if (isset($param))
         {
           $link = 'https://qualoperadora.info/consulta';
           $dados = array
           (
-           'tel'=> preg_replace('/[^0-9]/','',$param['code'].$param['tel']),
-            'bto'=>'Descobrir Operadora'
+           'tel'=> preg_replace('/[^0-9]/','',$param['code'].$param['tel'])
+           ,'bto'=>'Descobrir Operadora'
           );
           $dados = http_build_query($dados,null,"");
           $ch = curl_init($link);
+          var_dump($link);
           curl_setopt($ch, CURLOPT_REFERER, $link);
           curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
           curl_setopt($ch, CURLOPT_MAXREDIRS, 3);
@@ -51,15 +54,14 @@ class TelefoneForm extends TPage
           curl_setopt($ch, CURLOPT_POST, true);
           curl_setopt($ch,CURLOPT_POSTFIELDS, $dados);
           //curl_setopt($ch, CURLOPT_COOKIEJAR, $arquivo);
-            $html = curl_exec($ch); 
-            $txt  = preg_split("/:/", $html);
-        
-            print_r($txt);
-            
+          var_dump($ch);
+          $html = curl_exec($ch);
+          var_dump($html);
+          $txt  = preg_split("/:/", $html);
+          var_dump($txt);
+          print_r($txt);
           exit();
-           curl_close($ch);
+          curl_close($ch);
         }
     }
-
-
 }
